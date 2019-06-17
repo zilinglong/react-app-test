66b9142f85e69909a58c5ec7a7c4d2f63c776514
mengmingxiu <mengmingxiu@100tal.com>
2019-06-17 16:35:17 +0800

    React JSX

--- src/App.js
+++ src/App.js
@@ -7,9 +7,9 @@
 //     <div className="App">
 //       <header className="App-header">
 //         <img src={logo} className="App-logo" alt="logo" />
-//         <p>
+//         <li>
 //           Edit <code>src/App.js</code> and save to reload.
-//         </p>
+//         </li>
 //         <a
 //           className="App-link"
 //           href="https://reactjs.org"
@@ -24,12 +24,38 @@
 import React from "react";
 class App extends React.Component {
   render() {
+    const i = 1;
+    const myInlineStyle = {
+      fontSize: 20,
+      color: "#f00"
+    };
+    const arr = ["<h5>菜鸟教程<h5>", "<h5>学的不仅是技术，更是梦想！</h5>"];
     return (
       <div>
-        <h1>jjjj</h1>
-        <h2>2222</h2>
-        <h2>这里是测试react应用的demo</h2>
-        <h4>{this.props.date.toLocaleString()}</h4>
+        <h1>react 元素</h1>
+        <h4>本地时间：{this.props.date.toLocaleString()}</h4>
+        <ol>
+          <li>
+            自定义属性使用：<span data-attr="someValue">这里使用了自定义属性data-attr</span>
+          </li>
+          <li>表达式使用：{1 + 1}</li>
+          <li>
+            三元运算符使用：<span>{i === 1 ? "i===1" : "i!==1"}</span>
+          </li>
+          <li>
+            react推荐使用内联样式：
+            <p style={myInlineStyle}>这里使用了内联样式-引入变量</p>
+            <p style={{fontSize:20,color:'0f0'}}>这里使用了内联样式-直接定义【要用两个花括号】</p>
+          </li>
+          <li>
+            这里是注释的使用：注释必须包含在“花括号{}”中，而且只能用/**/，不能用两个斜线//
+            {/*这里是注释*/}
+          </li>
+          <li>
+            在jsx中插入数组：
+            {arr}
+          </li>
+        </ol>
       </div>
     );
   }
